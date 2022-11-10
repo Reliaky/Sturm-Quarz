3b4e750a3be33c03fb59e43708f0e86e72270626
Steine1234 <116732609+Steine1234@users.noreply.github.com>
2022-11-10 16:12:14 +0100

    Add files via upload

---
+++ assignment/Week1Task5.py
@@ -0,0 +1,13 @@
+a=int(input())
+b=int(input())
+c=int(input())
+if (a>(b+c)) or (b>(a+c)) or (c>(a+b)):
+    print("No triangle")
+elif a==b==c:
+    print("equilateral")
+elif a==b or a==c or b==c:
+    print("isosceles")
+else:
+    print("scalene")
+
+
