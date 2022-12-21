6f735854ba26498f32d3625ff4d66cea44f9936d
ChrisGugel <117267868+ChrisGugel@users.noreply.github.com>
2022-12-21 10:08:26 +0100

    Add files via upload

---
+++ assignment/ChrisGugel/Assignment03/A03_T1.py
@@ -0,0 +1,28 @@
+# Task 01
+
+# Subtask 1
+
+def clients():
+    accounts = {'a': 0, 'b': 0, 'c': 0}
+
+
+clients()
+
+# Subtask 2
+acc.update()  # add dictionary pairs
+acc.popitem()  # remove dictionary pairs
+
+# Subtask 3 & 4
+def incbal(self, x):
+    x = input('Increase which account?')
+    y = input('Increase by what amount?')
+    return
+
+def decbal(self, x):
+    x = input('Decrease which account?')
+    y = input('Decrease by what amount?')
+    if a,b,c < 0
+        print('Insufficient funds!')
+    else
+        continue
+    return
---
+++ assignment/ChrisGugel/Assignment03/A03_T2.py
@@ -0,0 +1,7 @@
+# Task 2
+
+# Subtask 1
+class Customer:
+    def __init__(self, client, balance):
+        self.client = client
+        self.balance = balance
